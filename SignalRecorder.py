9a464b24c0785e7a896fe4fdb25b067159f85655
JachiOnuoha <jachiuzo@gmail.com>
2023-09-14 16:56:50 -0700

    [WIP] working example using starter pyrtlsdr code

--- SignalRecorder.py
+++ SignalRecorder.py
@@ -1,11 +1,18 @@
-from rtlsdr import RtlSdr
+from pylab import *
+from rtlsdr import *
 
 sdr = RtlSdr()
 
 # configure device
-sdr.sample_rate = 2.048e6  # Hz
-sdr.center_freq = 70e6     # Hz
-sdr.freq_correction = 60   # PPM
+sdr.sample_rate = 2.4e6
+sdr.center_freq = 372e6
 sdr.gain = 'auto'
 
-print(sdr.read_samples(512))
+samples = sdr.read_samples(256*1024)
+sdr.close()
+
+# use matplotlib to estimate and plot the PSD
+psd(samples, NFFT=1024, Fs=sdr.sample_rate/1e6, Fc=sdr.center_freq/1e6)
+xlabel('Frequency (MHz)')
+ylabel('Relative power (dB)')
+savefig("myplot.png")
