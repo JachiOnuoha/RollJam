60d16b8bb51959526cb71696c8e1163ee39f3739
JachiOnuoha <jachiuzo@gmail.com>
2023-09-18 00:40:30 -0700

    [WIP] Signal processor

--- SignalProcessor.py
+++ SignalProcessor.py
@@ -1,3 +1,5 @@
+from typing import Tuple, Union
+from matplotlib import pyplot
 from pylab import *
 from enum import Enum
 from scipy import signal
@@ -8,13 +10,17 @@ class SignalProcessor:
     def __init__(self) -> None:
         pass
         
-    def apply_lowpass_filter(signal_array:list[complex64], cutoff_frequency: float, sampling_rate: float, poles: int=5, custom_filter: SignalFilter = None)->list[complex64]:
+    def apply_lowpass_filter(self, signal_array: Union[list[complex64], list[float]], 
+                             cutoff_frequency: float, 
+                             sampling_rate: float, 
+                             poles: int=5, 
+                             custom_filter: SignalFilter = None)->Union[list[complex64], list[float]]:
         """
         Apply a lowpass filter to a signal array.
 
         Parameters
         ----------
-        signal_array : list[complex64]
+        signal_array : list[complex64] | list[float]
             The signal, in array form, to be filtered
         cutoff_frequency : float
             The desired cutoff frequency of the filter
@@ -27,21 +33,67 @@ class SignalProcessor:
 
         Returns
         -------
-        list[complex64]
+        list[complex64] | list[float]
             A filtered version of the original signal array 
         """
         filter = custom_filter
 
         # Create butterworth lowpass filter if no custom filter or non-lowpass filter is provided
-        if filter == None | filter.filter_type != FilterType.LOWPASS:
+        if filter == None:
             sos = signal.butter(N=poles, Wn=cutoff_frequency, btype='low',fs=sampling_rate, output='sos')
             filter = sos
         
         filtered_signal_array = signal.sosfiltfilt(filter, signal_array)
         return filtered_signal_array
 
-    def apply_highpass_filter(signal_array:list[complex64], cutoff_frequency: float, sampling_rate: float, poles: int, custom_filter: list[float]= None)->list[complex64]:
+    def apply_highpass_filter(self, signal_array:list[complex64], cutoff_frequency: float, sampling_rate: float, poles: int, custom_filter: list[float]= None)->list[complex64]:
         pass
 
-    def apply_bandpass_filter(signal_array:list[complex64], cutoff_frequency: float, sampling_rate: float, poles: int, custom_filter: list[float]= None)->list[complex64]:
+    def apply_bandpass_filter(self, signal_array:list[complex64], cutoff_frequency: float, sampling_rate: float, poles: int, custom_filter: list[float]= None)->list[complex64]:
         pass
+
+    #TODO: Move this to an example class 
+    @staticmethod
+    def generate_sample_tone_with_noise(sampling_rate: float, time_shift_vector_size: float, tone_frequency: float)->Tuple[list[float], list[float]]:
+        """_summary_
+
+        Parameters
+        ----------
+        sampling_rate : float
+            _description_
+        time_shift_vector_size : float
+            _description_
+        tone_frequency : float
+            _description_
+
+        Returns
+        -------
+        Tuple[list[float], list[float]]
+            _description_
+        """
+        time_shift_vector = np.arange(time_shift_vector_size)/sampling_rate
+        noise = 0.2*np.random.randn(len(time_shift_vector))
+        signal = np.sin(2 * np.pi * tone_frequency * time_shift_vector)
+        signal_with_noise = signal + noise
+        return signal_with_noise, time_shift_vector
+    
+    #TODO: Move this to an example class 
+    def plot_time_domain_signal(self, time_shift_vector: list[float], signal: list[float], sampling_rate: float, file_name: str)-> None:
+        """_summary_
+
+        Parameters
+        ----------
+        time_shift_vector : list[float]
+            _description_
+        signal : list[float]
+            _description_
+        sampling_rate : float
+            _description_
+        file_name : str
+            _description_
+        """
+        plot(time_shift_vector*sampling_rate, signal)
+        # plt.locator_params(axis='x', nbins=10)
+        xlabel("Time Domain")
+        ylabel("Amplitude")
+        savefig(file_name)
--- main.py
+++ main.py
@@ -1,10 +1,29 @@
-from SignalRecorder import *
+from pylab import *
 from SDR import *
+from SignalRecorder import *
+from SignalProcessor import *
 
-my_sdr = SDR()
-rx_file_name = "Test-Recording-File"
-signal_recorder = SignalRecorder(my_sdr)
-my_signal_array = signal_recorder.record_samples(signal_array_size=256*1024, center_frequency=372e6, sdr_gain='auto')
-signal_recorder.write_signal_to_file(my_signal_array, rx_file_name)
-new_signal_array = signal_recorder.read_signal_from_file(rx_file_name)
-signal_recorder.generate_psd_plot(new_signal_array, "plots/{}-Plot-1".format(rx_file_name))
+
+
+# my_sdr = SDR()
+# rx_file_name = "Test-Recording-File"
+# signal_recorder = SignalRecorder(my_sdr)
+# my_signal_array = signal_recorder.record_samples(signal_array_size=256*1024, center_frequency=372e6, sdr_gain='auto')
+# signal_recorder.write_signal_to_file(my_signal_array, rx_file_name)
+# new_signal_array = signal_recorder.read_signal_from_file(rx_file_name)
+# signal_recorder.generate_psd_plot(new_signal_array, "plots/{}-Plot-1".format(rx_file_name))
+
+# mock_signal, mock_time_shift_vector = SignalProcessor.generate_sample_tone_with_noise(2.4e6, 1024*1000, 372e6)
+# SignalProcessor.plot_time_domain_signal(mock_time_shift_vector[:200], mock_signal[:200], 2.4e6)
+my_sig_processor = SignalProcessor()
+sampling_rate = 1e6
+time_shift_vector = np.arange(1000)/sampling_rate # time vector
+
+signal_one = np.sin(2 * np.pi * 10e3 * time_shift_vector)
+signal_two = np.sin(2 * np.pi * 20e3 * time_shift_vector)
+signal_sum = np.sin(2 * np.pi * 10e3 * time_shift_vector) + np.sin(2 * np.pi * 20e3 * time_shift_vector)
+filtered_signal = my_sig_processor.apply_lowpass_filter(signal_sum, 20e3, sampling_rate)
+
+my_sig_processor.plot_time_domain_signal(time_shift_vector, signal_two, sampling_rate, "20MHz-Signal")
+# my_sig_processor.plot_time_domain_signal(time_shift_vector, signal_sum, sampling_rate, "Summed-10MHz-and-20MHz")
+my_sig_processor.plot_time_domain_signal(time_shift_vector, filtered_signal, sampling_rate, "Filtered-Summed-10MHz-and-20MHz")
